98ac089d622616fa302868da7a35d3b91a83429a
Rostislav Rabiec <rosta.rabiec@icloud.com>
2023-01-26 09:31:11 +0100

    Version 0.0.1 Creating game Tic, Tac, Toe Created greeting function

---
+++ projekt_22.py
@@ -0,0 +1,40 @@
+"""
+projekt_22.py: Druhý projekt do Engeto Online Python Akademie
+
+author: Rostislav Rabiec
+email: rosta.rabiec@icloud.com
+discord: Rostislav R.#9305
+"""
+
+# Vytvoř pozdrav a vysvětli pravidla hry
+# Herní pole
+# Samotný chod hry
+
+
+def main():
+    create_greetings()
+    create_gamefield()
+    play_game()
+
+
+def create_greetings():
+    separator = 40 * "="
+    greeting = f"Welcome to Tic Tac Toe\n{separator}"
+    rules = f"""
+GAME RULES:
+Each player can place one mark (or stone)
+per turn on the 3x3 grid. The WINNER is
+who succeeds in placing three of their
+marks in a:
+* horizontal
+* vertical
+* diagonal row
+{separator}
+Let's start the game
+"""
+    return print(greeting, rules)
+
+
+
+
+
